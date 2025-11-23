d3caf34a15372d0b407dda6cb1b410b7a9bb35ff
estuda-neto <clodoaldo.britodev@gmail.com>
2025-11-23 18:37:51 -0300

    feat-backend: configuring a connection to a PostgreSQL database using Sequelize ORM.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+    imports: [SequelizeModule.forRoot({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: "craft_sales",
+        models: [],
+        autoLoadModels: true,
+        synchronize: true
+    })]
+})
+export class DatabaseModule { }
